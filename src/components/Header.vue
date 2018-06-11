ed67a8d508aaf5b9d29eccbf480bdbf780155a42
Alfredo Irarrazaval <airarrazaval@users.noreply.github.com>
2018-06-11 16:35:18 +0200

    added official language logo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,7 @@
     <nav class="navbar bg-fao" v-if="show">
       <div class="container-fluid">
         <div class="navbar-header">
-          <a class="navbar-brand fao-logo-white" href="http://www.fao.org" target="_blank">
+          <a :class="['navbar-brand', getLogoClass()]" href="http://www.fao.org" target="_blank">
           </a>
         </div>
       </div>
@@ -14,9 +14,23 @@
 <script>
 export default {
   name: 'AppHeader',
+  data() {
+    return {
+      languages: ['en', 'fr', 'es', 'ru', 'zh', 'ar'],
+    };
+  },
+  methods: {
+    getLogoClass() {
+      const lang = this.languages.indexOf(this.lang) !== -1 ? this.lang : 'en';
+      return `fao-logo-${lang}`;
+    },
+  },
   computed: {
     show() {
       return this.$route.query.header !== '0';
+    },
+    lang() {
+      return this.$route.query.lang || 'en';
     },
   },
 };
@@ -41,8 +55,31 @@ $logo-size: $logo-width $logo-height;
     margin-left: -($logo-width / 2) !important;
     width: $logo-width;
     height: $logo-height;
-    background: url('/static/img/fao-logo-white.svg') no-repeat;
     background-size: $logo-size;
+
+    &.fao-logo-en {
+      background: url('/static/img/FAO_logo_en.svg') no-repeat;
+    }
+
+    &.fao-logo-fr {
+      background: url('/static/img/FAO_logo_fr.svg') no-repeat;
+    }
+
+    &.fao-logo-es {
+      background: url('/static/img/FAO_logo_es.svg') no-repeat;
+    }
+
+    &.fao-logo-ru {
+      background: url('/static/img/FAO_logo_ru.svg') no-repeat;
+    }
+
+    &.fao-logo-zh {
+      background: url('/static/img/FAO_logo_zh.svg') no-repeat;
+    }
+
+    &.fao-logo-ar {
+      background: url('/static/img/FAO_logo_ar.svg') no-repeat;
+    }
   }
 }
 </style>
