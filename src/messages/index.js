6aee20ca41f6bcff3d7f08bca63b1770ca1a57ee
Alfredo Irarrazaval <airarrazaval@users.noreply.github.com>
2018-06-18 17:45:39 +0200

    added new messages

--- src/messages/index.js
+++ src/messages/index.js
@@ -1,18 +1,25 @@
-import notFound from './errors/404.json';
+import badRequest from './errors/400.json';
 import unauthorized from './errors/401.json';
+import notFound from './errors/404.json';
+import timeout from './errors/408.json';
 import submitted from './submitted.json';
 
 export { submitted as SubmittedMessages };
-export { notFound as NotFoundMessages };
+export { badRequest as BadRequestMessages };
 export { unauthorized as UnauthorizedMessages };
+export { notFound as NotFoundMessages };
+export { timeout as TimeoutMessages };
 
-const ErrorMessages = { notFound, unauthorized };
+const ErrorMessages = {
+  badRequest,
+  unauthorized,
+  notFound,
+  timeout,
+};
+
 export { ErrorMessages };
 
 export default {
   submitted,
-  errors: {
-    notFound,
-    unauthorized,
-  },
+  errors: ErrorMessages,
 };
