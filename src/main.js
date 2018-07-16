b5fb33d79b0296fdad16b5f8e26b671f4c070624
Alfredo Irarrazaval <airarrazaval@users.noreply.github.com>
2018-07-16 15:43:51 +0200

    updated to formiojs and added moment.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import moment from 'moment';
 import App from './App';
 import router from './router';
 import resources from './resources';
 import messages from './messages';
+
+window.moment = moment;
 
 Vue.config.productionTip = false;
 
